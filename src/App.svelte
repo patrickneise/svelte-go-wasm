d974058b03117acaf76e7f61f072a254ea3e2ed0
Patrick Neise <patrick.neise@gmail.com>
2021-01-11 20:29:41 -0500

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -13,16 +13,13 @@
 </script>
 
 <body>
-  <main class="flex-grow w-full">
-    <p>WASM Test</p>
-
-    <textarea bind:value={jsoninput} cols="80" rows="20" />
-    <input
-      id="button"
-      type="submit"
-      name="button"
-      value="pretty json"
-      on:click={handleJson(jsoninput)} />
+  <main>
+    <div class="container w-10/12 mx-auto">
+    <h1 class="py-8 text-xl text-center">Svelte-Go-WASM</h1>
+    
+    <textarea bind:value={jsoninput} placeholder="JSON String" cols="80" rows="10" />
+    <button type="button" on:click={handleJson(jsoninput)} class="block px-4 py-2 mx-auto my-5 bg-blue-400 w-30 text-gray-50">Pretty JSON</button>
     <textarea id="jsonoutput" name="jsonoutput" cols="80" rows="20" />
+  </div>
   </main>
 </body>
