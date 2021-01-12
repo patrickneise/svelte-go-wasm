35cdd28304e22120ad812c2233ff6d798e362e79
Patrick Neise <patrick.neise@gmail.com>
2021-01-12 01:47:22 +0000

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -17,9 +17,11 @@
     <div class="container w-10/12 mx-auto">
     <h1 class="py-8 text-xl text-center">Svelte-Go-WASM</h1>
     
+    <div class="mx-auto">
     <textarea bind:value={jsoninput} placeholder="JSON String" cols="80" rows="10" />
     <button type="button" on:click={handleJson(jsoninput)} class="block px-4 py-2 mx-auto my-5 bg-blue-400 w-30 text-gray-50">Pretty JSON</button>
     <textarea id="jsonoutput" name="jsonoutput" cols="80" rows="20" />
+    </div>
   </div>
   </main>
 </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,6 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
     require('tailwindcss-debug-screens'),
   ],
 }
